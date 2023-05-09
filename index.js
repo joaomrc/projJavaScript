d15aa9ff68e31639688ef88183b25d0348261062
joaomrc <120732181+joaomrc@users.noreply.github.com>
2023-05-09 10:25:08 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+const urlIPMA_Distritos = 'http://site.api.espn.com/apis/site/v2/sports/basketball/nba/news';
+// const urlIPMA = 'https://api.ipma.pt/open-data/forecast/meteorology/cities/daily/1141600.json';
+
+// Obter referencia para o elemento <select>
+var select = document.querySelector('select'); 
+
+// Obter referencia para o elemento <section>
+var section = document.querySelector('section'); 
